3c3a65637574c9313a4081f1d47bab2f63237147
Steven Dombrowski <dombrowskis153@yahoo.com>
2024-06-12 09:44:40 -0400

    Chapter 19 done

---
+++ src/main/java/org/launchcode/codingevents/AuthenticationFilter.java
@@ -0,0 +1,55 @@
+package org.launchcode.codingevents;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.launchcode.codingevents.controllers.AuthenticationController;
+import org.launchcode.codingevents.data.UserRepository;
+import org.launchcode.codingevents.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class AuthenticationFilter implements HandlerInterceptor {
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    AuthenticationController authenticationController;
+
+    //The whitelist gets defined before the preHandle
+    private static final List<String> whitelist = Arrays.asList("/login", "/register", "/logout", "/css");
+
+    //This method checks what is whitelisted and what is not
+    private static boolean isWhitelisted(String path){
+        for(String pathRoot : whitelist){
+            if(path.startsWith(pathRoot)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) throws IOException{
+        if(isWhitelisted(request.getRequestURI())){
+            return true;
+        }
+
+        HttpSession session = request.getSession();
+        User user = authenticationController.getUserFromSession(session);
+
+        //The user is logged in
+        if (user != null){
+            return true;
+        }
+        //The user is NOT logged in
+        response.sendRedirect("/login");
+        return false;
+    }
+}
